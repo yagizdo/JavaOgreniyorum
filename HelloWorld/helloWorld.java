73287b5fbae655cddd2163b2c4ea517b6f6d6a0f
yagizdo <dokumaciyagiz@gmail.com>
2019-07-04 14:35:01 +0300

    This project print Hello World

--- HelloWorld/helloWorld.java
+++ HelloWorld/helloWorld.java
@@ -1,6 +1,5 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("This is my first java Project");
         System.out.println("Hello World"); 
     }
 }
